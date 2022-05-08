e73a5d3646c9c2fb406fad017a3a3196c2f91ffd
Koros <ivan.koros2@gmail.com>
2022-05-08 07:41:13 -0400

    Initial commit

---
+++ Array diff.py
@@ -0,0 +1,15 @@
+one = [ 1, [ 1, 1 ] ]
+two = [ 2, [ 2, 2 ] ]
+
+print(len(one))
+print(len(two))
+
+def same_structure_as(original,other):
+    if len(original) == len(original):
+        return all(len)
+
+
+
+
+
+same_structure_as(one, two)
